fe1abc8617808a2fddc05cbbb9ca7d7937eab6c7
Konye <isnantointerista@gmail.com>
2018-04-23 12:29:34 +0700

    masalah pertama terselesaikan

--- web_app/app.py
+++ web_app/app.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_pyfile('setting.py')
 
     @app.route('/')
     def index():
-        return 'halo flask, ini hot reload yaaa !!!!'
+        return render_template('index.html', TITTLE='nitnot')
+
+    @app.route('/about')
+    def about():
+        return render_template('about.html', TITTLE='nitnot')
+
+    @app.route('/news')
+    def news():
+        return render_template('news.html', TITTLE='nitnot')
 
     return app
 
