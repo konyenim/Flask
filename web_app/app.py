ff27bdf31534a31ba54fede06fdced917c3baef6
Konye <isnantointerista@gmail.com>
2018-04-26 15:34:49 +0700

    masalah terselesaikan

--- web_app/app.py
+++ web_app/app.py
@@ -1,12 +1,26 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, String
+
 
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('setting.py')
 
+    db = SQLAlchemy(app)
+    class Page(db.Model):
+        __tablename_='page'
+        id = Column(Integer, primary_key=True)
+        contents = Column(String)
+
+
+    db.create_all()
+
+
     @app.route('/nitnot')
     def nitnot():
-        return render_template('index.html', TITTLE='nitnot')
+        page = Page.query.filter_by(id=1).first()
+        return render_template('index.html', TITTLE='nitnot', CONTENT=page.contents)
 
     @app.route('/')
     def index():
@@ -19,6 +33,19 @@ def create_app():
     @app.route('/news')
     def news():
         return render_template('news.html', TITTLE='nitnot')
+
+    @app.route('/testdb')
+    def testdb():
+        import psycopg2
+
+        con = psycopg2.connect('dbname=Flask user=devuser password=devpassword host=postgres')
+        cur = con.cursor()
+
+        cur.execute('select * from page;')
+
+        id, title =cur.fetchall()
+        con.close()
+        return 'output table page: {} - {}'.format(id, title)
 
     return app
 
